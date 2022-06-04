e6f413f6a14cb08868e5529cb0b48b9ff4f7ddf3
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-06-04 00:39:29 -0400

    create nix expression for home packages

---
+++ home.nix
@@ -0,0 +1,24 @@
+# REF
+# https://stackoverflow.com/questions/50802880/reproducible-nix-env-i-with-only-nix-no-nixos
+
+# USE
+# nix-env -if home.nix
+# nix-env -e home
+
+let
+  pkgs = import <nixpkgs> {};
+  inherit (pkgs) buildEnv;
+in buildEnv {
+  name = "home";
+  paths = [
+    pkgs.bat
+    pkgs.exa
+    pkgs.figlet
+    pkgs.git
+    pkgs.lolcat
+    pkgs.neovim
+    pkgs.ripgrep
+    pkgs.starship
+    pkgs.zoxide
+  ];
+}
