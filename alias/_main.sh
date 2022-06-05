17f82644417b8f574b3a110162b56ec4936b45a5
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-06-04 20:31:16 -0400

    refactor lifecycle functions and use `command`

--- alias/_main.sh
+++ alias/_main.sh
@@ -6,54 +6,86 @@
 # should i be using functions for everything instead?
 # https://www.gnu.org/software/bash/manual/bash.html#Aliases
 
-#
-# SCRIPT START
-#
-
 # get list of files
 # sort each file
 # report
 # load good
 # cleanup
 
+#
+# SCRIPT START
+#
+
 OK_PATH="/tmp/OK.alias"
 ERR_PATH="/tmp/ERR.alias"
-touch $OK_PATH
-touch $ERR_PATH
+
+main () {
+  setup
+  check_file
+  report
+  teardown
+}
+
+#
+# logic
+#
 
 # (alias line) -> bool
 check_line () {
   # BUG: `tr` naive impl
   local cmd=$(echo "$1" | cut -d "=" -f 2 | tr -d \"\')
-  # no quotes to grab first WORD only
+  # no quotes to grab first word only
   command -v $cmd &> /dev/null
 }
 
 # (alias file) -> good/bad aliases files
 check_file () {
-  local input="./terraform.sh" # hardcoded 4 now
+  local input="./misc.sh" # hardcoded 4 now
   # [ -n "$line" ] to scan files that don't end with `\n`
   while IFS="" read -r line || [ -n "$line" ]; do
     if check_line "$line"; then
       # load alias now to validate dependent aliases
       # alias tf="terraform"
       # alias tfp="tf plan"
-      eval "$line"
-      echo "$line" >> $OK_PATH
+      command eval "$line"
+      command echo "$line" >> $OK_PATH
     else
-      echo "$line" >> $ERR_PATH
+      command echo "$line" >> $ERR_PATH
     fi
   done < "$input"
 }
 
-cleanup () {
+#
+# lifecycle
+#
+
+setup () {
+  check_path () {
+      if [ -e "$1" ]; then
+        command echo "ERR: $OK_PATH is not empty; aborting"
+        command exit 1
+      fi
+  }
+  check_path $OK_PATH
+  check_path $ERR_PATH
+  command touch $OK_PATH
+  command touch $ERR_PATH
+}
+
+report () {
+  command echo OK
+  command cat $OK_PATH
+  command echo ERR
+  command cat $ERR_PATH
+}
+
+teardown () {
   command rm $OK_PATH
   command rm $ERR_PATH
 }
 
-check_file
-echo OK:
-cat $OK_PATH
-echo ERR:
-cat $ERR_PATH
-cleanup
+#
+#
+#
+
+main
