5d459c28a4619e3a2bb7dd5837cac04caf9cb7e0
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-06-04 22:46:55 -0400

    update and organize shell.sh aliases

--- alias/shell.sh
+++ alias/shell.sh
@@ -1,19 +1,26 @@
 # shell tools
+alias cat="bat"
+alias f5="exec $SHELL"
 alias ls="exa"
 alias ll="ls -l"
 alias la="ls -la"
-alias cat="bat"
 alias rm="rm -i"
-alias f5="exec $SHELL"
+alias vim="nvim"
 
 # ergo keybinds
 alias ..="cd .."
 alias a="z"
 alias ai="zi"
-alias o="nvim"
-alias e="clear;"
+alias o="clear ;"
+alias e="vim"
 alias u="la"
+
+# combo ergobinds
+alias erc="e ~/.*rc" # "edit rc"
+alias ou="o u"
+alias ot="o t"
+alias out="o t; u"
+alias t="date \"+%H : %M\" | figlet | lolcat"
 alias ut="la --sort newest"
 alias uu="ll"
 alias uut="ll --sort newest"
-alias t="date \"+%H : %M\" | figlet | lolcat"
