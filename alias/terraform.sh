9c86f7154dccacdb3dea29bd40be519b1881e11c
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-06-09 11:28:00 -0400

    add terraform aliases re: -out tfplan

--- alias/terraform.sh
+++ alias/terraform.sh
@@ -2,10 +2,14 @@ alias tf="terraform"
 alias tfe="tfenv"
 
 alias tfv="tf validate"
+
 alias tfp="tf plan"
 alias tfpo="tf plan -out tfplan"
 alias tfpd="tf plan -destroy"
+alias tfpdo="tf plan -destroy -out tfplan"
+
 alias tfa="tf apply"
+alias tfao="tf apply tfplan"
 alias tfu="tf apply -auto-approve"
 alias tfd="tf destroy"
 
