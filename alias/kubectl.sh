29c11ba1c3d8de3fcdd740c824921f44e0ebeb5b
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-07-31 14:15:10 -0400

    alias for kubectl metrics

--- alias/kubectl.sh
+++ alias/kubectl.sh
@@ -3,6 +3,9 @@ alias kns="kubens"
 
 alias k="kubectl"
 alias kg="kubectl get"
-alias kap="kubectl get po -A -o wide"
+alias kap="kubectl get po -A"
 alias kaf="kubectl apply -f"
 alias kdf="kubectl delete -f"
+
+alias ktn="kubectl top node"
+alias ktp="kubectl top pod --sort-by memory"
